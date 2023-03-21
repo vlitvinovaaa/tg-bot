5254e844c5a2fe5cc818aa8aa489e53dc3f6bcfa
vlitvinova-at-062869851462 <vlitvinova95@gmail.com>
2023-03-21 20:28:33 +0200

    nothing is working fuck

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 const TelegramBot = require('node-telegram-bot-api');
-const randomWords = require('random-words');
 const Jimp = require('jimp');
+const fs = require('fs');
 
-const bot = new TelegramBot('6280948390:AAE57h9He1yq7zDU5_SpFMtFU3CEy-Ohki8', { polling: true });
+// Create a bot that uses 'polling' to fetch new updates
+const token = '6280948390:AAE57h9He1yq7zDU5_SpFMtFU3CEy-Ohki8';
+const bot = new TelegramBot(token, { polling: true });
 
+// Listen for incoming messages
 bot.on('message', async (msg) => {
-    // Check if message contains a photo
     if (msg.photo) {
-        // Get information about the largest photo sent
-        const photo = msg.photo.pop();
-
         try {
-            // Download the photo from Telegram's servers
-            const path = await bot.downloadFile(photo.file_id, 'photos');
+            // Get the file ID of the largest photo
+            const fileId = msg.photo[msg.photo.length - 1].file_id;
 
-            // Generate a random caption using the random-words package
-            const caption = randomWords({ min: 3, max: 10 }).join(' ');
+            // Download the photo
+            const filePath = await bot.downloadFile(fileId, './photos');
+            const imageBuffer = await fs.promises.readFile(filePath);
 
-            // Load the photo into Jimp and add the caption
-            const image = await Jimp.read(path);
-            const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-            image.print(font, 10, 10, caption);
+            // Load the image
+            const image = await Jimp.read(imageBuffer);
 
-            // Save the modified photo to a file
-            const modifiedPath = `modified-${photo.file_unique_id}.jpg`;
-            await image.writeAsync(modifiedPath);
+            // Load the Lobster font
+            const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
 
-            // Send the modified photo back to the user
-            bot.sendPhoto(msg.chat.id, modifiedPath);
-        } catch (err) {
-            console.error(err);
+            // Add the text to the image
+            const text = 'LOL OMFG';
+            const textWidth = Jimp.measureText(font, text);
+            const x = (image.getWidth() - textWidth) / 2;
+            const y = image.getHeight() - 40;
+            image.print(font, x, y, {
+                text,
+                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+            });
+
+            // Send the edited image back to the user
+            bot.sendPhoto(msg.chat.id, await image.getBufferAsync(Jimp.MIME_JPEG));
+        } catch (error) {
+            console.error(error);
+            bot.sendMessage(msg.chat.id, 'An error occurred while processing your image. Please try again later.');
         }
     }
 });
